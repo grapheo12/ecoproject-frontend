04a7c1dfe8d122602cc91508959063b822bf1067
Shubham Mishra <smishra99.iitkgp@gmail.com>
2020-06-09 22:54:52 +0530

    Initial Commit
    Signed-off-by: Shubham Mishra <smishra99.iitkgp@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {LineChart, XAxis, YAxis, Legend, Tooltip, CartesianGrid, Line} from 'recharts';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Chip from '@material-ui/core/Chip';
+import MenuItem from '@material-ui/core/MenuItem';
+import Slider from '@material-ui/core/Slider';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+
+const useStyles = makeStyles({
+  root: {
+    width: 300
+  },
+  formControl: {
+    margin: 2,
+    minWidth: 120,
+    maxWidth: 300
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  chip: {
+    margin: 2
+  },
+  noLabel: {
+    marginTop: 3
+  }
+});
 
 function App() {
+  var [data, dataSetter] = useState([{dum: 1, my: 2}]);
+  var [range, rangeSetter] = useState([25, 70]);
+  var [maxDay, maxDaySetter] = useState(200);
+  var [plots, plotSetter] = useState([]);
+  var [colorChart, colorSetter] = useState({dum: '#000000', my: '#ffffff'});
+  const classes = useStyles();
+  useEffect(() => {
+    axios.get(`http://localhost:5000/all?start=${range[0]}&end=${range[1]}`)
+      .then(({ data }) => {
+        dataSetter(() => (data));
+        Object.keys(data[0]).forEach((elt) => {
+          colorChart[elt] = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)});
+          console.log(colorChart);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios.get('http://localhost:5000/numdays')
+      .then(({data}) => {
+        console.log(data)
+        maxDaySetter(() => (Number(data.numDays)));
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
+  function rangeChanger(event, newValue){
+    console.log(newValue);
+    rangeSetter(() => (newValue));
+    axios.get(`http://localhost:5000/all?start=${range[0]}&end=${range[1]}`)
+      .then(({ data }) => {
+        dataSetter(() => (data));
+        console.log(Object.keys(data[0]));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart
+        data = {data}
+        width = {600}
+        height = {400}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3.3" />
+        <Tooltip />
+        <XAxis dataKey="day" label="Day" type="number" domain={['dataMin', 'dataMax']} />
+        <YAxis />
+        <Legend />
+        {
+          plots.map((plt) => (
+            <Line type="monotone" dataKey={plt} stroke={(() => {console.log(colorChart[plt]); return colorChart[plt];})()} dot={{r: 1}} />
+          ))  
+        }
+      </LineChart>
+
+      <div className={classes.root}>
+        <Typography gutterBottom>
+          Date Range:
+        </Typography>
+
+        <Slider
+          value={range}
+          onChange={rangeChanger}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          max={maxDay}
+          
+        />
+
+      </div>
+
+      <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Plots</InputLabel>
+          <Select
+            labelId="plots-label"
+            multiple
+            input={<Input />}
+            value={plots}
+            onChange={(event) => {
+              console.log(event.target.value);
+              plotSetter(event.target.value);
+            }}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {selected.map((val) => (
+                  <Chip key={val} label={val} className={classes.chip} />
+                ))}
+              </div>
+            )}
+          >
+            {Object.keys(data[0]).map((k) => (
+              <MenuItem key={k} value={k} className={classes.chip}>{k}</MenuItem>
+            ))}
+            
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
